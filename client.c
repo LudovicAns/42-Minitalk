d577c61d2c78a990f5dda36cbb9536903ee797ec
LudovicAns <ludovicanselin@gmail.com>
2021-08-25 19:02:17 +0200

    🔨 Structuration of project

--- client.c
+++ client.c
@@ -1,90 +1,26 @@
-#include <stdio.h>
-#include <signal.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lanselin <lanselin@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/25 18:57:14 by lanselin          #+#    #+#             */
+/*   Updated: 2021/08/25 18:57:14 by lanselin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int ft_strlen(char *string)
-{
-	int count;
+#include "minitalk.h"
 
-	count = 0;
-	while (string[count])
-		count++;
-	return (count);
-}
-
-char *char_to_bin(int c)
-{
-	char	*bin;
-	int		i;
-
-	bin = malloc(sizeof(char) * 9);
-	if (!bin)
-		return (NULL);
-	i = 7;
-	while (i >= 0 && c != 0)
-	{
-		if (c % 2 == 1)
-			bin[i] = '1';
-		else
-			bin[i] = '0';
-		c /= 2;
-		i--;
-	}
-	while (i >= 0)
-		bin[i--] = '0';
-	bin[8] = '\0';
-	return (bin);
-}
-
-int send_signals(int pid, char *bin)
-{
-	int state;
-
-	state = 0;
-	while (*bin && state >= 0)
-	{
-		if (*bin == '0')
-			state = kill(pid, SIGUSR1);
-		else
-			state = kill(pid, SIGUSR2);
-		bin++;
-		usleep(10);
-	}
-	return (state);
-}
-
-int error(char *cause)
-{
-	while (*cause)
-		write(STDOUT_FILENO, &*cause++, 1);
-	return (1);
-}
-
+/*
+*	Welcome to the main function of client.
+*
+*	@param	int		argc	-	Argument count.
+*	@param	char	**argv	-	Parameters given to client by user.
+*
+*	@return	nothing
+*/
 int	main(int argc, char **argv)
 {
-	int		state;
-	int 	pid;
-	char	*string;
-	char	 *test_bin;
-
-	if (argc != 3)
-		return (error("Error: Invalid Argument\n"));
-	string = argv[2];
-	pid = atoi(argv[1]);
-	state = 0;
-	while (*string && state >= 0)
-	{
-		test_bin = char_to_bin(*string);
-		state = send_signals(pid, test_bin);
-		free(test_bin);
-		string++;
-	}
-	if (state >= 0)
-		state = send_signals(pid, "00000000");
-	if (state == -1)
-		return (error("Error: Can't send signals\n"));
-	return (0);
+	// Rework is comming ...
 }
--- minitalk.h
+++ minitalk.h
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lanselin <lanselin@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/25 18:49:39 by lanselin          #+#    #+#             */
-/*   Updated: 2021/08/25 18:49:39 by lanselin         ###   ########.fr       */
+/*   Created: 2021/08/25 18:45:59 by lanselin          #+#    #+#             */
+/*   Updated: 2021/08/25 18:45:59 by lanselin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include <signal.h>
+# include "libft.h"
 
 /*
-*	Welcome to the main function of server.
-*
-*	@return	nothing
+** Colors
+** -
+** This section is completely optional,
+** I just used it to "pimp" my code !
 */
-int	main(void)
-{
-	// Rework is comming ...
-}
+# define ANSI_COLOR_RED		"\x1b[31m"
+# define ANSI_COLOR_BLUE	"\x1b[34m"
+# define ANSI_COLOR_GREEN	"\x1b[32m"
+# define ANSI_COLOR_YELLOW	"\x1b[33m"
+# define ANSI_COLOR_RESET	"\x1b[0m"
+
+#endif
